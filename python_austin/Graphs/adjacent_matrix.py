5a70df7b6f54d18b2046a138bafe10b388a44d60
aparrish22 <austinparrish202@gmail.com>
2023-01-09 06:18:16 -0600

    matrix update

--- python_austin/Graphs/adjacent_matrix.py
+++ python_austin/Graphs/adjacent_matrix.py
@@ -1,10 +1,19 @@
+import numpy as np
+
 class Node:
+    
+    edges = []
     
     def __init__(self, element):
         self.has_edge = False
         self.element = element
         
+    def insert_edge(self, node, weight):
+        self.edges.append({node : weight})
+        self.has_edge = True
+            
+        
 class Matrix:
     
     def __init__(self):
-        pass
+        self.matrix = np.zeros( (3,3), dtype=np.int64) # placeholder for now

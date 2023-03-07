bd9c9f8c436ed43a924c5623e8935a27b2b1b44b
aparrish22 <austinparrish202@gmail.com>
2023-03-06 23:17:26 -0600

    reupload

---
+++ java_austin/IndexedLinkedList/IndexedLinkedListTest.java
@@ -0,0 +1,18 @@
+package java_austin.IndexedLinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/*
+ * JUnit Testing Implementation of iLL with Java
+ */
+
+public class IndexedLinkedListTest {
+    
+    @Test
+    public void testAdd() {
+        // skeleton added via source action on active code
+
+    }
+}
